4691f7a1f4d435d55d604f84acf3df33fb8efac5
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-26 21:44:50 +0530

    binding at socket 0

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::net::TcpListener;
+
 use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse, dev::Server};
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -8,16 +10,15 @@ async fn health_check(_req: HttpRequest) -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub fn run(address: &str) -> Result<Server, std::io::Error>{
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error>{
     let server = HttpServer::new(|| {
         App::new()
         .route("/", web::get().to(greet))
         .route("/{name}", web::get().to(greet))
         .route("/health_check", web::get().to(health_check))
     })
-    .bind(address)?
+    .listen(listener)?
     .run();
 
     Ok(server)
 }
-
