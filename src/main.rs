cc42ac2840dde335ab13d3213f2a55fe43a74827
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-26 21:45:10 +0530

    passing listener to run()

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 #[cfg(test)]
 mod tests;
 
+use std::net::TcpListener;
+
 use newsletter::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()>{
     let address = "localhost:3000";
-    run(address)?.await
+    let listener = TcpListener::bind(address).unwrap();
+    run(listener)?.await
 }
-
