0ed83f08843b012d60dc9d2abb3fb5c61a1da46f
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-27 00:56:42 +0530

    parsing body

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 use std::net::TcpListener;
-
 use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse, dev::Server};
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    name: String, 
+    email: String,
+}
+
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
@@ -10,11 +16,16 @@ async fn health_check(_req: HttpRequest) -> impl Responder {
     HttpResponse::Ok()
 }
 
+async fn handle_subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error>{
     let server = HttpServer::new(|| {
         App::new()
         .route("/", web::get().to(greet))
         .route("/{name}", web::get().to(greet))
+        .route("/subscriptions", web::post().to(handle_subscribe))
         .route("/health_check", web::get().to(health_check))
     })
     .listen(listener)?
