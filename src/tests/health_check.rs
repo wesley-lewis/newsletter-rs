72aabc9aaafd5ee3cd70c28b82ec21c0ca7e724b
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-26 21:45:29 +0530

    tested health_check path

--- src/tests/health_check.rs
+++ src/tests/health_check.rs
@@ -1,21 +1,27 @@
+use std::net::TcpListener;
+
 use reqwest;
 
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
+    let address = spawn_app();
     
     let client = reqwest::Client::new();
+    dbg!(&address);
     let response = client 
-        .get("localhost:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request");
     assert!(response.status().is_success());
-    assert_eq!(Some(0), response.content_length());
+    assert_eq!(Some(19), response.content_length());
 }
 
-async fn spawn_app() {
-    let server = newsletter::run("localhost").expect("Failed to bind.");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").unwrap();
+    let port = listener.local_addr().unwrap().port();
+    let server = newsletter::run(listener).expect("Failed to bind.");
 
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
